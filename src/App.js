7840236973dfb29733e84dfb473a753aaaada8ba
kenryu <jryu1521@gmail.com>
2024-04-02 17:09:52 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Button from "./Button";
 import styles from "./App.module.css"
-import { useState } from "react";
+import { useState , useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log("render");
+  useEffect(() => {
+    console.log("call the api");
+  }, [])
+
+  useEffect(() => {
+    console.log("search for count");
+  }, [counter])
+
   return (
     <div>
       <h1>{counter}</h1>
